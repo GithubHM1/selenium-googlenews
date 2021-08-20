d81b35dc9292493f7f0adb0d468c34702a6e2d81
qnods <edward@qnods.com>
2021-08-20 19:10:16 +0800

    add PATH for Mac as per recommeded by YM

--- googlenews3a.py
+++ googlenews3a.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 import time
 import pandas as pd
+import datetime
 
-PATH = "D:\chromedriver.exe"
+PATH = "./chromedriver.exe" # For Windows
+# PATH = "./chromedriver" # For MacOS
 driver = webdriver.Chrome(PATH)
 articles =[]
 url =''
@@ -34,10 +36,26 @@ def craft_url(query, region, start, end):
     driver.get(url)
     print(url)
 
-def convert_date(value):
+def convert_pubdate(value):
+    modifier = 0
     old_date = value.split()
-    new_date = old_date[2] + "-" + month[old_date[1]] + "-" + old_date[0]
-
+    day_differences = modifier*int(old_date[0])
+    today = datetime.date.today()
+    date_reducer = datetime.timedelta(days = day_differences)
+    if 'hour' in value or 'hours' in value:
+        new_date = (today - date_reducer).isoformat()
+    elif 'day' in value or 'days' in value:
+        modifier=1
+        new_date = (today - date_reducer).isoformat()
+    elif 'week' in value or 'weeks' in value:
+        modifier=7
+        new_date = (today - date_reducer).isoformat()
+    elif 'month' in value or 'months' in value:
+        modifier=30
+        new_date = (today - date_reducer).isoformat()
+    else:
+        new_date = old_date[2] + "-" + month[old_date[1]] + "-" + old_date[0]
+    
     return new_date
 
 
@@ -47,7 +65,7 @@ def append_articles():
         title = post.find_element_by_css_selector("div[class='JheGif nDgy9d']").text
         excerpt = post.find_element_by_css_selector("div[class='Y3v8qd']").text
         source = post.find_element_by_css_selector("div[class='XTjFC WF4CUc']").text
-        date = convert_date(post.find_element_by_xpath('.//span[@class="WG9SHc"]/span').text)
+        date = convert_pubdate(post.find_element_by_xpath('.//span[@class="WG9SHc"]/span').text)
         link = post.find_element_by_xpath('.//a').get_attribute("href")
         article = { 
             "query": main_query,
@@ -76,14 +94,13 @@ def click_next():
 
 craft_url('semiconductor','US','1/1/2020','1/31/2020')
 
-# This is for loop multiple pages
-
 while status == True:
     append_articles()
     click_next()
 
-# This is for single page
 # append_articles()
 
 df = pd.DataFrame(articles)
 df.to_csv('semiconductor.csv')
+
+driver.close()
